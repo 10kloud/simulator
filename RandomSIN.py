e46b88c28982c06032676e1872938db925bc2fbb
Mattia <mattia.ceschin@gmail.com>
2021-05-13 15:21:16 +0200

    Simulatore
    Aggiunta la simulazione dei dati anche per la pressione(interna), umidità(esterna)  e temperatura(esterna)

--- RandomSIN.py
+++ RandomSIN.py
@@ -1,4 +1,4 @@
 import math
 class RandomSIN:
-    def Random(lenght, startPosition, interval, time):
-        return math.sin(time*lenght+startPosition)*interval+interval
+    def Random(lenght, startPosition, interval, interval1, time):
+        return math.sin(time*lenght+startPosition)*interval+interval1
